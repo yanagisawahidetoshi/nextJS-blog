6f75a37191d07652e71dd73e984e8685ad5cd945
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-21 00:59:02 +0900

    CSS追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { NextPage } from "next";
+import styles from "../styles/index.module.css";
 
 const Index: NextPage = () => {
-  return <h1>BLOG</h1>;
+  return <h1 className={styles.title}>BLOG</h1>;
 };
 
 export default Index;
