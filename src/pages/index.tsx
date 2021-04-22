0964c0398af0d0725511e6a317115bc5aac9cb9c
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-22 18:37:35 +0900

    記事一覧を表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
 import { NextPage } from "next";
 import styles from "../styles/index.module.css";
+import { IPost, mockPosts } from "../models/posts";
 
 const Index: NextPage = () => {
-  return <h1 className={styles.title}>BLOG</h1>;
+  return (
+    <>
+      <h1 className={styles.title}>BLOG</h1>
+      <div>
+        {mockPosts.map((post: IPost) => {
+          return (
+            <section>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+              <p>{post.createdAt}</p>
+            </section>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Index;
