880cc5b4fed868db3cd590828e1369e7342420d9
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-25 17:28:16 +0900

    build時エラー修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import styles from "../styles/index.module.css";
 import { IPost, mockPosts } from "../models/posts";
 
-const Index: NextPage = ({ posts }) => {
+const Index: NextPage<any> = ({ posts }) => {
   return (
     <>
       <h1 className={styles.title}>BLOG</h1>
@@ -29,10 +29,10 @@ const Index: NextPage = ({ posts }) => {
 };
 
 export const getStaticProps = async () => {
-  const key = {
+  const key: any = {
     headers: { "X-API-KEY": process.env.API_KEY },
   };
-  const res = await fetch(`${process.env.API_BASE_URL}blog`, key)
+  const res: any = await fetch(`${process.env.API_BASE_URL}blog`, key)
     .then((res) => res)
     .catch((err) => console.log(err));
   const data = await res.json();
