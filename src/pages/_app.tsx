35d05d01434cf0716a5fea5b2a95d23c07a6ebc3
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-26 02:35:21 +0900

    themeProvider導入

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle``;
+
+const theme = {
+  colors: {
+    primary: "#3498db",
+  },
+};
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
