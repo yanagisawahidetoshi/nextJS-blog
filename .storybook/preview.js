92caa2e7fb2cccdd13767cd65ed025d011dff8b2
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-27 03:17:46 +0900

    storybookでnext/imageを使えるようにモック化させる

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,32 @@
+import * as nextImage from "next/image";
+
+Object.defineProperty(nextImage, "default", {
+  configurable: true,
+  value: (props) => {
+    const { width, height } = props;
+    const ratio = (height / width) * 100;
+    return (
+      <div
+        style={{
+          paddingBottom: `${ratio}%`,
+          position: "relative",
+        }}
+      >
+        <img
+          style={{
+            objectFit: "cover",
+            position: "absolute",
+            minWidth: "100%",
+            minHeight: "100%",
+            maxWidth: "100%",
+            maxHeight: "100%",
+          }}
+          {...props}
+        />
+      </div>
+    );
+  },
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,4 +36,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
