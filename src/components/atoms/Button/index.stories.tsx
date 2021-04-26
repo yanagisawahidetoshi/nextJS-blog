f3c27a3f3918410e402ebab3d6566eb7452fa878
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-26 23:48:15 +0900

    src/components/atoms/Button リファクタ

---
+++ src/components/atoms/Button/index.stories.tsx
@@ -0,0 +1,8 @@
+import Button from "./";
+
+export default {
+  title: "components/atoms/Button",
+  component: Button,
+};
+
+export const Basic = () => <Button href="./">ボタン</Button>;
---
+++ src/components/atoms/Button/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Button = styled.a`
+  border: 1px solid #bfc8d2;
+  border-radius: 25px;
+  color: #3e465b;
+  display: inline-block;
+  font-size: 10px;
+  font-weight: 700;
+  margin: 0 10px;
+  padding: 5px 15px;
+  text-decoration: none;
+  text-transform: uppercase;
+
+  &:hover {
+    border-color: #000;
+  }
+`;
