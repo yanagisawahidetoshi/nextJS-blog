b371225039755a3719a5e3e407730890404a8f35
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-27 21:30:22 +0900

    エラー修正

--- src/components/atoms/HeaderImage/index.tsx
+++ src/components/atoms/HeaderImage/index.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Link from "next/link";
-
 import Wrapper from "./Wrapper";
-import imgSrc from "../../../../public/profile.png";
 
 const HeaderImage = () => {
   return (
     <Wrapper>
       <Link href="/">
         <a>
-          <img src={imgSrc} alt="" />
+          <img src="/profile.png" alt="" />
         </a>
       </Link>
     </Wrapper>
