625518cff1a5a2e94d893ea65b1eb599b389cee6
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-27 17:20:29 +0900

    src/components/atoms/Header 修正

--- src/components/atoms/Header/Link.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import GatsbyLink from "gatsby-link";
-
-const Link = styled(GatsbyLink)`
-  color: ${(props) => props.theme.colors.primary};
-  text-decoration: none;
-`;
-
-export default Link;
--- src/components/atoms/Header/index.tsx
+++ src/components/atoms/Header/index.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import userConfig from "../../../config";
+
+import Link from "next/link";
 
 import Container from "../Container";
 import HeaderImage from "../HeaderImage";
 import Social from "../Social";
 import H1 from "../H1";
 import P from "./P";
-import Link from "./Link";
+import Anchor from "./Anchor";
 import Wrapper from "./Wrapper";
 
-const Header = ({ config }) => {
-  const { author, description, social } = config;
-
+const Header = () => {
   return (
     <Container>
       <Wrapper>
-        {userConfig.showHeaderImage && <HeaderImage />}
+        <HeaderImage />
         <H1>
-          <Link to="/">{author}</Link>
+          <Link href="/">
+            <Anchor>Hidetoshi Yanagisawa</Anchor>
+          </Link>
         </H1>
-        <P>{description}</P>
-        {social && (
-          <Social
-            website={social.website}
-            github={social.github}
-            twitter={social.twitter}
-            linkedin={social.linkedin}
-          />
-        )}
+        <P>Full-stack Web Developer</P>
+        <Social
+          website="https://afternoon-peak-11400.herokuapp.com/"
+          github="https://github.com/yanagisawahidetoshi"
+          twitter="https://twitter.com/ultrasevenstar1"
+          linkedin="https://www.linkedin.com/in/hidetoshi-yanagisawa-a6063b132/"
+        />
       </Wrapper>
     </Container>
   );
