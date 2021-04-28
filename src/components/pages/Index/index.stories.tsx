e90fb17fcfeb152bd843e1e0458dc835caf239fb
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-28 10:47:01 +0900

    トップページリファクタ

---
+++ src/components/pages/Index/index.stories.tsx
@@ -0,0 +1,9 @@
+import PageIndex from "./";
+import { mockPosts } from "../../../models/posts";
+
+export default {
+  title: "components/pages/PageIndex",
+  component: PageIndex,
+};
+
+export const Basic = () => <PageIndex posts={mockPosts} />;
