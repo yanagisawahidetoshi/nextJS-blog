5e4de0864ee040ad8b3abc68c49f9800774dab0e
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-04-29 01:45:26 -1000

    storyshots導入

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  roots: ["<rootDir>"],
+  moduleFileExtensions: ["js", "ts", "tsx", "json"],
+  testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$",
+    "node_modules/(?!@shotgunjed)/",
   ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.[t|j]sx?$": "babel-jest",
+    "^.+\\.[tj]sx?$": "./jest.transform.js",
+    "^.+\\.mdx?$": "@storybook/addon-docs/jest-transform-mdx",
   },
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+  },
+  testEnvironment: "jsdom",
 };
