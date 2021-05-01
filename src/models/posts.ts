0988b552cd2ff90f2631a9cf655a3af5802f3a50
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-05-01 00:07:46 -1000

    interface修正

--- src/models/posts.ts
+++ src/models/posts.ts
@@ -1,27 +1,35 @@
 export interface IPost {
-  id: number; //記事ID
+  id: string; //記事ID
   title: string; // 記事タイトル
   content: string; // 記事の内容
   createdAt: string; // 記事作成日時
+  description: string;
+  kv: string;
 }
 
 export const mockPosts: Array<IPost> = [
   {
-    id: 1,
+    id: "a1",
     title: "タイトル",
     content: "内容",
     createdAt: "2021-01-01",
+    description: "ディスクぷション",
+    kv: "",
   },
   {
-    id: 2,
+    id: "b2",
     title: "タイトル2",
     content: "内容2",
     createdAt: "2021-02-02",
+    description: "ディスクぷション",
+    kv: "",
   },
   {
-    id: 3,
+    id: "c3",
     title: "タイトル3",
     content: "内容3",
     createdAt: "2021-03-03",
+    description: "ディスクぷション",
+    kv: "",
   },
 ];
