88e7f60bcc9de00bf6779ce5eb6725e2f365a3e2
柳沢英俊 <yanagisawa@ultrasevenstar.com>
2021-05-01 18:55:03 -1000

    img置換

--- src/components/atoms/HeaderImage/index.tsx
+++ src/components/atoms/HeaderImage/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import Wrapper from "./Wrapper";
+import Image from "next/image";
 
 const HeaderImage = () => {
   return (
     <Wrapper>
       <Link href="/">
         <a>
-          <img src="/profile.png" alt="" />
+          <Image
+            src="/profile.png"
+            width={135}
+            height={150}
+            alt="HidetoshiYanagisawaのプロフィール画像"
+          />
         </a>
       </Link>
     </Wrapper>
